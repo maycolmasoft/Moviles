b53ac3af9034b853b603adb1d7f59411abfdf501
Steven <Steven@STEVEN>
2023-01-17 19:46:27 -0500

    Agregar archivos de proyecto.

---
+++ Moviles/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace Moviles
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private const string Url = "http://192.168.1.100/moviles/post.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<Moviles.Datos> _post;
+
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            obtener();
+        }
+
+
+        public async void obtener() {
+
+            var content = await client.GetStringAsync(Url);
+            List<Moviles.Datos> posts = JsonConvert.DeserializeObject<List<Moviles.Datos>>(content);
+            _post = new ObservableCollection<Moviles.Datos>(posts);
+            Lista.ItemsSource = _post;
+
+
+        }
+
+        private void btnInsertar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Registro());
+
+        }
+    }
+}
---
+++ Moviles/Registro.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Moviles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsertar_Clicked(object sender, EventArgs e)
+        {
+
+            try
+            {
+            
+            WebClient cliente = new WebClient();
+            var parametros = new System.Collections.Specialized.NameValueCollection();
+            parametros.Add("codigo", txtId.Text);
+            parametros.Add("nombre", txtNombre.Text);
+            parametros.Add("apellido", txtApellido.Text);
+            parametros.Add("edad", txtEdad.Text);
+
+           
+            cliente.UploadValues("http://192.168.1.100/moviles/post.php", "POST", parametros);
+
+            Navigation.PushAsync(new MainPage());
+
+            }
+            catch (Exception ex){
+
+                DisplayAlert("ALERTA", ex.Message,"cerrar");
+            }
+
+
+        }
+
+        private void btnCancelar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+    }
+}
